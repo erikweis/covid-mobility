5af3a71b4c744f3f859cc1d9e5c315d2c82eaa09
erikweis <32458904+erikweis@users.noreply.github.com>
2021-11-13 19:03:41 -0500

    example test

---
+++ test_job.py
@@ -0,0 +1,11 @@
+from job import Job
+from location import Location
+
+def test_job_initialization():
+
+    loc = Location(3,4,10)
+    j = Job(3,loc,salary=50000)
+    
+    assert j.idx == 3
+    assert j.location == loc
+    assert j.salary == 50000
